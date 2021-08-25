ca308092e7721e272ab249b76477ed317495b938
Rohan D. Shukla <rohanataglowid@gmail.com>
2021-08-25 17:56:59 +0530

    image/file upload finished

--- task-manager/src/routes/user.js
+++ task-manager/src/routes/user.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
+const multer = require('multer')
+const sharp = require('sharp')
 const router = new express.Router()
 
 const handleUserLogin = async (req, res) => {
@@ -46,7 +48,7 @@ router.post('/users', async (req, res) => {
         await user.save()
         const token = await user.generateAuthToken()
         res.status(201).send({user,token})
-    } catch (e) {
+    }  catch (e) {
         res.status(400).send(e)
     }
 })
@@ -102,6 +104,55 @@ router.delete('/users/me', auth, async (req, res) => {
         res.send(user)
     } catch (e) {
         res.status(500).send()
+    }
+})
+
+
+// Upload user avatar
+const upload = multer({
+    limits: { fileSize: 1000000 },
+    fileFilter(req,file,cb) {
+        if(file.mimetype !== 'image/jpeg') {
+            return cb(new Error('Please upload an image file'))
+        }
+
+        cb(undefined, true)
+    }
+})
+
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req,res) => {
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250}).png().toBuffer()
+    req.user.avatar = buffer
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({error: error.message})
+})
+
+// DELETE User avatar
+router.delete('/users/me/avatar', auth, async (req, res) => {
+   try {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+   } catch(e) {
+       res.status(500).send()
+   }
+})
+
+
+// GET user avatar URL
+router.get('/user/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        if(!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/png')
+        res.send(user.avatar)
+    } catch(e) {
+        res.status(404).send()
     }
 })
 
